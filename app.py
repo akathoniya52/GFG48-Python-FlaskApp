f615c6a21f97ad0d4140be80f7b0a9bf7318ee34
sudhanshuvlog <pandeysudhanshuup@gmail.com>
2026-09-19 10:59:36 +0530

    updated app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        return 'Hi hi GFG48 19-09-2026abcdefgggg12333356666666'
+        return 'Hi hi GFG41 19-09-2026abcdefgggg12333356666666'
 
     return app
 
