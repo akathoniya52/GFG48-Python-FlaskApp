156508bd54d43791306fd540b9c29b5c1cd67b44
sudhanshuvlog <pandeysudhanshuup@gmail.com>
2026-09-20 10:57:26 +0530

    fixed the issues

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM python:3.11-slim
 WORKDIR /app
-COPY . . 
-RUN pip install -r requirements.txt
+COPY requirements.txt requirements.txt
+COPY app.py app.py
+RUN groupadd -r gfg && \
+    useradd -r -g gfg pythongfg && \  
+    pip install -r requirements.txt && \
+    chown -R pythongfg:gfg /app
 
 EXPOSE 80
-USER root
+USER pythongfg
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask import Flask
-import pandas
-import numpy
 
 def create_app():
     app = Flask(__name__)
@@ -10,22 +8,6 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        for i in range(10):
-            print("inside home function")
-        return 'Hi hi GFG48 19-09-2026 Hi hello1234'
-
-    @app.route('/')
-    def home():
-        print("inside home function")
-        while True:
-            print("inside home function")
-        return 'Hi hi GFG48 19-09-2026 Hi hello1234'
-
-    @app.route('/test')
-    def home1():
-        print("inside home1 function")
-        while True:
-            print("inside home1 function")
         return 'Hi hi GFG48 19-09-2026 Hi hello1234'
 
     return app
